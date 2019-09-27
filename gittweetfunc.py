668b88d5808695f66a1821ef8531175a40131278
Cagri Yoruk <cyoruk@bu.edu>
2019-09-27 18:20:40 -0400

    initialize

--- gittweetfunc.py
+++ gittweetfunc.py
@@ -2,12 +2,12 @@
 
 # Import the twitter library
 import tweepy
-# Define a function to get the tweets.
+# Define a function to get
 def get_tweet():
   twitter_feed = []
   # Authorize 
-  auth = tweepy.OAuthHandler("2vmFnAMVIO8NfLE6QlFrf4Rc3", "q8xCQGZx0YHC8cQngdnmpaQ5DeKJAx1fS0GHkH9b06tUp3T6Yi")
-  auth.set_access_token("1171098217959677952-U7TzAWtJ98ZP7iLDNctKoWNBVvmn6C","muelgRZWemk3tojNB3Bz2zr0ckc6hEQC51N5rhOR1Y9Km")
+  auth = tweepy.OAuthHandler("XXXXXXXXXX", "XXXXXXXXXX")
+  auth.set_access_token("XXXXXXXXXX","XXXXXXXXXX")
   auth_tweet = tweepy.API(auth)
   while 1:
     print("What do you want to do?","\n","1-Get tweets from my feed","\n","2-Get tweets from a user")
