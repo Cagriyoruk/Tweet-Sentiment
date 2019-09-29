831e98070f5067f5f4bf4a6c2173454fe6a8ac33
Cagri Yoruk <cyoruk@bu.edu>
2019-09-29 15:27:43 -0400

    Monkey_test_char

--- Tweet-SentimentFinal.py
+++ Tweet-SentimentFinal.py
@@ -17,12 +17,12 @@ def get_tweet():
   auth_tweet = tweepy.API(auth)
   while 1:
     print("What do you want to do?","\n","1-Get tweets from my feed","\n","2-Get tweets from a user")
-    select = int(input())
-    if select == 1:
+    select = input()
+    if select == '1':
       numberoftweets = int(input("How many tweets do you want?"))
       tweets = auth_tweet.home_timeline(count = numberoftweets)
       break
-    elif select == 2:
+    elif select == '2':
       numberoftweets = int(input("How many tweets do you want?"))
       name = input("Enter the user name:")
       tweets = auth_tweet.user_timeline(id = name,count = numberoftweets)
